e37bfe8739349fc8113053d23666a22c9fa9e288
gruzdev-as <gruzdev-as@yandex.ru>
2021-05-26 11:43:39 +0800

    Added canvas

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 ''' Main file for app ''' 
 
 import sys 
+
 from PyQt5 import QtWidgets, QtCore
+
 import design
 
+
+import matplotlib
+matplotlib.use('QT5Agg') # backend for qt app
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import(
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar,
+    )
+
+class MplCanvase(FigureCanvas):
+    ''' The class describe a figure for import them into the apps widget ''' 
+
+    def __init__(self):
+        ''' Executed when the class is instantiated '''
+
+        fig = Figure()
+        self.axes = fig.add_subplot()
+
+        super().__init__(fig)
+        
 
 class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
@@ -11,6 +34,30 @@ class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
         ''' Init a file for acess to vars and methods in design.py'''
         super().__init__()
         self.setupUi(self) # design init
+
+        # Canvas setting 
+
+        self.canvas = MplCanvase()
+        self.canvas_lay = QtWidgets.QVBoxLayout(self.widget)
+        self.toolbar = NavigationToolbar(self.canvas, self.widget)
+        self.canvas_lay.addWidget(self.canvas) # Add Canvas obj to Widget
+        self.canvas_lay.addWidget(self.toolbar) # Add toolbar to Widget
+        self.canvas.axes.clear()
+        self.canvas.axes.grid()
+
+
+        #Events
+        self.Open_button.clicked.connect(self.open_file)
+        self.Save_button.clicked.connect(self.save_file)
+
+
+    def open_file(self):
+        ''' Open file for analysis ''' 
+        None
+    
+    def save_file(self):
+        ''' Save file as JSON ''' 
+        None
 
 
 def main():
