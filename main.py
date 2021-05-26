030d0b59b296ea16ebf97a00acc50a5856681991
gruzdev-as <gruzdev-as@yandex.ru>
2021-05-26 11:09:32 +0800

    Created design file, main.py file, .gitignore

---
+++ main.py
@@ -0,0 +1,24 @@
+''' Main file for app ''' 
+
+import sys 
+from PyQt5 import QtWidgets, QtCore
+import design
+
+
+class Application(QtWidgets.QMainWindow, design.Ui_MainWindow):
+
+    def __init__(self):
+        ''' Init a file for acess to vars and methods in design.py'''
+        super().__init__()
+        self.setupUi(self) # design init
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = Application()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
