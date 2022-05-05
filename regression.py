96963e0c3720b8a3fa566b757e670281ba2e7d4e
gruzdev-as <gruzdev-as@yandex.ru>
2022-05-05 12:35:52 +0800

    regression module created

---
+++ regression.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np 
+from random import choices
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+class NewRegressionModel():
+    
+    def __init__(self, dataframe:pd.DataFrame):
+        
+        self.df = dataframe
+        self.equation = None
+        self.model = None
+        self.accuracy = None
+        
+        self.vars = {
+            'equation': self.equation,
+            'model' : self.model,
+            'accuracy': self.accuracy
+        }
+        
+        
+    def create_model(self):
+        '''Create and evaluate a regression model'''
+        
+        test_areas = choices(self.df.section_area.value_counts().index, k=2)
+        
+        train_df = self.df.drop(
+            self.df[self.df.section_area == test_areas[0]].index
+            )
+        X_train = train_df.section_area.values.reshape(-1, 1)
+        y_train = train_df.max_force
+        
+        self.model = LinearRegression().fit(X_train, y_train)
+        
+        # Evaluation
+        
+        test_df = self.df[
+            (self.df.section_area == test_areas[0]) 
+            |  
+            (self.df.section_area == test_areas[1])
+            ]
+        X_test = test_df.section_area.values.reshape(-1, 1)
+        y_test = test_df.max_force
+        
+        y_predicted = self.model.predict(X_test)
+        self.accuracy = r2_score(y_pred=y_predicted, y_true=y_test)
+        X = self.model.coef_[0]
+        Y = self.model.predict
+        self.equation = f'Max Force = {X} * Section Area + {Y}'
+        
+        
+        
+        
+        
+    
+    
+
+    
+    
+    
